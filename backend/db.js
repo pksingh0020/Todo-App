e0ac8a4313e1651c2adb8b1fa2dd44f01e607ec3
pksingh0020 <92779167+pksingh0020@users.noreply.github.com>
2024-05-25 13:53:48 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const  mongoose = require("mongoose");
 
 
 
-mongoose.connect("mongodb+srv://adis:Adispass@cluster0.tzcs3vt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+mongoose.connect
 
 const todosSchema = mongoose.Schema({
     title: String,
